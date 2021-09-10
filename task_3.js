e0e756601ad0cd0eb8c3c6802567eb0965fd94fa
AlexE1987 <evsikovad1987@gmail.com>
2021-09-10 11:16:51 +0400

    refactor: remove arguments length check and additional condition

--- task_3.js
+++ task_3.js
@@ -1,9 +1,6 @@
 Array.prototype.myFilter = function myFilter(callback, thisArg) {
   let filteredArray = this.reduce((newArray, element, index) => {
-    if (arguments.length > 1) {
-      if (callback.call(thisArg, element, this, index)) newArray.push(element);
-    }
-    if (callback.call(this, element, this, index)) newArray.push(element);
+    if (callback.call(thisArg, element, this, index)) newArray.push(element);
     return newArray;
   }, []);
 
